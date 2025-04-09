4f120d19430fa28a65a725ef679597cbd6d340a7
peterjames3 <kamauchief97@gmail.com>
2025-04-09 15:36:21 +0300

    fix structureTool issu : lacking exported member

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,7 @@
 
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
+//import { structure } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
@@ -20,7 +20,7 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({ structure }),
+    structure,
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
